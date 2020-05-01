5dc962f53aa566df45dc57b0e9ab42a9d7bdf338
kqian5 <kycqian1005@gmail.com>
2020-04-30 22:09:01 -0400

    hi

--- code/model.py
+++ code/model.py
@@ -81,9 +81,7 @@ class Model(tf.keras.Model):
 		localization_out = img
 		for layer in self.localization:
 			localization_out = layer(localization_out)
-        
-        out = transformer(img, tf.reshape(localization_out, shape = (-1, 2, 3)))
-
+        out = transformer(img, tf.reshape(localization_out, shape=(-1, 6)))
 		# affine_transformation_layer = self.affine(img, tf.reshape(localization_out, shape = (-1, 2, 3)))
 
 		# transformed_vanilla = affine_transformation_layer(vanilla_out)
