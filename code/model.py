18ec3a55ab5b5fd5f67ee57b3affaf4a88101260
kqian5 <kycqian1005@gmail.com>
2020-05-01 00:03:16 -0400

    old code

--- code/model.py
+++ code/model.py
@@ -23,69 +23,62 @@ class Model(tf.keras.Model):
 		# they used 3x3x10 kernels, so we need to make sure the input is passed in with 10 channels.
 		# we can adjust these filter numbers
 
-		# self.vanilla = [
-		# 	Conv2D(64, 3, 1, input_shape=(hp.img_size, hp.img_size, 1), padding='same', activation="relu"),
-		# 	Conv2D(64, 3, 1, padding='same'),
-		# 	MaxPool2D(2),
-		# 	ReLU(),
+		self.vanilla = [
+			Conv2D(32, 3, 1, input_shape=(hp.img_size, hp.img_size, 1), padding='same', activation="relu"),
+			Conv2D(32, 3, 1, padding='same'),
+			MaxPool2D(2),
+			ReLU(),
 
-		# 	Conv2D(64, 3, 1, padding='same', activation="relu"),
-		# 	Conv2D(64, 3, 1, padding='same'),
-		# 	MaxPool2D(2),
-		# 	ReLU(),
+			Conv2D(32, 3, 1, padding='same', activation="relu"),
+			Conv2D(32, 3, 1, padding='same'),
+			MaxPool2D(2),
+			ReLU(),
 
-		# 	Dropout(0.3)
-		# ]
+			Dropout(0.3)
+		]
 
-		# self.localization = [
-		# 	Conv2D(32, 3, 1, input_shape=(hp.img_size, hp.img_size, 1), padding='same'),
-		# 	MaxPool2D(2),
-		# 	ReLU(),
+		self.localization = [
+			Conv2D(32, 3, 1, input_shape=(hp.img_size, hp.img_size, 1), padding='same'),
+			MaxPool2D(2),
+			ReLU(),
 
-		# 	Conv2D(32, 3, 1, padding='same'),
-		# 	MaxPool2D(2),
-		# 	ReLU(),
+			Conv2D(32, 3, 1, padding='same'),
+			MaxPool2D(2),
+			ReLU(),
 
-		# 	Dense(32, activation='relu'),
-		# 	Dense(6),
-		# ]
+			Dense(32, activation='relu'),
+			Dense(32),
+		]
 
-		# self.affine = AffineLayer()
+		self.affine = AffineLayer()
 
-		# self.multiply = Multiply()
-		self.transformer = SpatialTransformLayer()
+		self.multiply = Multiply()
 
 		self.head = [
 
 			# insert localization network here
 			Flatten(),
 
-			Dense(50, input_shape=(1, )),
+			Dense(50),
 			Dense(7, activation="softmax")
 		]
 
 	def call(self, img):
 		""" Passes input image through the network. """
-		# vanilla_out = img
-		# for layer in self.vanilla:
-		# 	vanilla_out = layer(vanilla_out)
-		# out = vanilla_out
-		# localization_out = img
-		# for layer in self.localization:
-		#     localization_out = layer(localization_out)
-		# print("localization")
-		# print(localization_out.shape)
+		vanilla_out = img
+		for layer in self.vanilla:
+			vanilla_out = layer(vanilla_out)
+		localization_out = img
+		for layer in self.localization:
+		    localization_out = layer(localization_out)
 		# out = transformer(tf.reshape(img, shape=(32, 48, 48, 1)), tf.reshape(localization_out, shape=(-1, 6)))
-		# # affine_transformation_layer = self.affine(img, tf.reshape(localization_out, shape = (-1, 2, 3)))
-		# print(out.shape)
-		# # transformed_vanilla = affine_transformation_layer(vanilla_out)
-		print(img.shape)
-		t_model = self.transformer
-		out = t_model.call(img)
-		print(out.shape)
+		affine_transformation_layer = self.affine(img, tf.reshape(localization_out, shape = (-1, 2, 3)))
+		print(affine_transformation_layer.shape)
+		# transformed_vanilla = affine_transformation_layer(vanilla_out)
+
 		for layer in self.head:
-		    out = layer(out)
-		return out
+		    transformed_vanilla = layer(transformed_vanilla)
+		return transformed_vanilla
 
 	@staticmethod
 	def loss_fn(labels, predictions):
